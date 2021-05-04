3bfa7ff67c62bff1280ff088a6d5ff1b8182ae02
veronica lopez <karla.veronicalt@hotmail.com>
2021-05-03 19:43:21 -0500

    Agregar archivos de proyecto.

---
+++ AnalizadorLexico/Compilador.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace AnalizadorLexico
+{
+    public class Compilador : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string error;
+        public string Error
+        {
+            get { return error; }
+            set
+            {
+                error = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Error"));
+            }
+        }
+
+        private string lineacodigo;
+        public string LineaCodigo
+        {
+            get { return lineacodigo; }
+            set
+            {
+                lineacodigo = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Codigo"));
+            }
+        }
+        public bool ValidateCode()
+        {
+            if (!LineaCodigo.EndsWith(";"))
+                Error = "Falta ;";
+            else
+                return true;
+        }
+
+    }
+}
